4db39c3aa3c30a4db3eb8249e8495160dc6a2524
vanilla-ice <valeraerohin97@mail.ru>
2016-04-17 16:04:22 +0300

    init

---
+++ src/reducers/reducer_book.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: The good parts', pages: 101},
+    {title: 'Harry Potter', pages: 39},
+    {title: 'The dark tower', pages: 50},
+    {title: 'Eloquent ruby', pages: 83}
+  ]
+}
